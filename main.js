db3816991030a5df38a1764f3863076cfd433fca
kant42 <kanan.great@gmail.com>
2018-03-09 13:34:42 +0530

    added more cities
    cities added and parts renamed

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2hvdXJ5YXNodWtsYSIsImEiOiJjamR5ajE2NTAwbTZpMzNqcnVndzh2dTJxIn0.AJ8EV6db3uJV93dX3lZYjQ';
 
 var chapters = {
-    'part-1': {
+    'intro': {
         center: [82.8, 23.88],
         zoom: 3.5,
         bearing: 0,
         pitch: 0,
     },
-    'part-2': {
+    'delhi': {
         center: [77.1016, 28.6375],
         zoom: 10,
         bearing: 0,
         pitch: 0,
     },
-    'part-3': {
+    'mumbai': {
         center: [72.8561, 19.0022],
         zoom: 11.1,
         bearing: 0,
         pitch: 52,
     },
-    'part-4': {
+    'pune': {
+    	center: [73.987,18.616],
+    	zoom: 9.2,
+    	bearing: 0,
+    	pitch: 0,
+    },
+    'chennai': {
+    	center: [80.239,13.146],
+    	zoom: 9.42,
+    	bearing: -23.20,
+    	pitch: 50.50,
+    },
+    'kolkata': {
+    	center: [88.809,22.496],
+    	zoom: 7.5,
+    	bearing: 0,
+    	pitch: 60,
+    },
+    'conclusion': {
         center: [82.8, 23.88],
         zoom: 4,
         bearing: 0,
@@ -53,7 +71,7 @@ window.onscroll = function() {
     }
 };
 
-var activeChapterName = 'part-1';
+var activeChapterName = 'intro';
 function setActiveChapter(chapterName) {
     if (chapterName === activeChapterName) return;
 
